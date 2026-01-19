2aa39c3b1058a290ff4000e71b3935878ab0cebc
TahsinaRashid <tahsinarashid123@gmail.com>
2026-01-19 10:30:43 +0600

    final

--- basic.py
+++ basic.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
-import  time
-driver = webdriver.Chrome()
-driver.get("https://practicetestautomation.com/practice-test-login/")
-driver.maximize_window()
-time.sleep(40)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver=webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+#login
+driver.find_element(By.NAME,value="user-name").send_keys("standard_user")
+driver.find_element(By.NAME,value="password").send_keys("secret_sauce")
+driver.find_element(By.ID,value="login-button").click()
+
+#add to cart
+driver.find_element(By.NAME,value="add-to-cart-sauce-labs-backpack").click()
+driver.find_element(By.NAME,value="add-to-cart-sauce-labs-bike-light").click()
+
+#view cart and checkout
+driver.find_element(By.CLASS_NAME,value="shopping_cart_link").click()
+driver.find_element(By.NAME,value="checkout").click()
+
+#logout
+driver.find_element(By.ID,value="react-burger-menu-btn").click()
+time.sleep(5)
+driver.find_element(By.ID,value="logout_sidebar_link").click()
+time.sleep(50)
